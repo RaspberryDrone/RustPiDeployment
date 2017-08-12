5bdd1e4336d07c7efe745c38db635fcea2a801bd
michaelpoellath <mpoellath@newstore.com>
2017-08-12 02:28:44 +0200

    inital

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RustPiDeployment
+  
+  ## How to use:
+  	
+   	- Export PI_HOST, PI_NAME and PI_PASSWORD as env variables
+    - Run the script with: python3 deploy.py deploy <path_of_project_folder>
+
+	The Script will SSH into your Raspi, move your code via sftp , compile and run it. 
+    At the moment you have to exit it with CTRL-C to kill the session.
---
+++ deploy.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+import paramiko
+import os
+from os.path import abspath, exists
+import argparse
+import glob
+
+PI_HOST = os.environ.get("PI_HOST")
+PI_NAME = os.environ.get("PI_NAME")
+PI_PASSWORD = os.environ.get("PI_PASSWORD")
+
+
+
+class DeployClient():
+    def __init__(self):
+        
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(
+        paramiko.AutoAddPolicy())
+        self.ssh.connect(PI_HOST, username=PI_NAME, 
+        password=PI_PASSWORD)
+        
+        channel = self.ssh.invoke_shell()
+        self.stdin = channel.makefile('wb')
+        self.stdout = channel.makefile('r')
+
+    #deploys rust project to pi for building and running
+    def deploy(self, path):
+        print('Deploying files...')
+        self._upload(path)
+        print('uploading done!')
+        self.stdin.write('cd '+path + ' && cargo build && cargo run\n')
+        self.stdin.flush()
+        self._read_command()
+        
+
+    def _read_command(self):
+        for line in self.stdout:
+            print(line)
+
+
+    def _upload(self, path):
+        files = glob.glob(path+'**', recursive=True)
+        for file in files:
+            self._sftp(file)
+            
+
+    def _sftp(self, file):
+        sftp = self.ssh.open_sftp()
+        if os.path.isdir(file):
+            
+            try: 
+                sftp.mkdir(file)
+            except IOError:
+                pass
+
+        else:
+            sftp.put(file,file)
+        sftp.close()
+
+
+def print_help(**args):
+    parser.print_help()
+
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+list_parser = subparsers.add_parser("deploy", help="Deploys a project to pi")
+list_parser.add_argument(
+    "path", help="The entities to list")
+list_parser.set_defaults(command="deploy")
+
+if __name__ == "__main__":
+    a = parser.parse_args()
+    args = vars(a)
+    print(args)
+    command = args.pop("command", None)
+    if not command:
+        parser.print_help()
+        exit(-1)
+    client = DeployClient()
+    getattr(client, command)(**args)        
+
+
